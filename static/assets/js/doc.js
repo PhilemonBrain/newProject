70e4fecba26f2544b598b5238b75a3cf0d6c8809
Kadet <techkadet@gmail.com>
2020-07-30 01:24:19 +0100

    added jinja template for docs page

--- static/assets/js/doc.js
+++ static/assets/js/doc.js
@@ -1,163 +1,162 @@
-/*jshint esversion: 6 */
+// /*jshint esversion: 6 */
 
-const currentTab = n => {
-    showTab(slideIndex = n);
-};
+// const currentTab = n => {
+//     showTab(slideIndex = n);
+// };
 
-const showTab = n => {
-    var i;
-    var x = document.querySelectorAll(".tab__content");
-    var tabs = document.querySelectorAll(".tab");
+// const showTab = n => {
+//     var i;
+//     var x = document.querySelectorAll(".tab__content");
+//     var tabs = document.querySelectorAll(".tab");
 
-    if (n > x.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = x.length;
-    }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    for (i = 0; i < tabs.length; i++) {
-        tabs[i].className = tabs[i].className.replace("active", "");
-    }
-    x[slideIndex - 1].style.display = "block";
-    tabs[slideIndex - 1].className += " active ";
-};
-
-
-
-// Copy Text
-
-const copyText = caller => {
-    parent = caller.parentElement;
-
-    textarea = parent.previousElementSibling;
-
-    textarea.select();
-
-    textarea.setSelectionRange(0, 99999);
-
-    document.execCommand("copy");
-
-};
-
-
-// Display platform API request based on Language
-
-
-
-platform = document.querySelectorAll(".req-platform");
-platform.forEach(item => {
-
-    item.onchange = () => {
-        codeEl = item.nextElementSibling;
-        console.log(codeEl.innerText);
-        if (item.value == "node") {
-            console.log(item.value);
-            codeEl.innerText = "node request goes here";
-
-        }
-
-        if (item.value == "php") {
-            console.log(item.value);
-
-            codeEl.innerText = "php request goes here";
-        }
-
-        if (item.value == "python") {
-            console.log(item.value);
-            codeEl.innerText = "python request goes here";
-        }
-    };
-});
-// console.log(platform);
-
-// output = document.querySelector(".req__code code");
-
-
-// platform.onchange = () => {
-
-
-//     if (platform.value == "node") {
-//         output.innerHTML = "node request goes here";
-
+//     if (n > x.length) {
+//         slideIndex = 1;
 //     }
-
-//     if (platform.value == "php") {
-//         output.innerHTML = "php request goes here";
+//     if (n < 1) {
+//         slideIndex = x.length;
 //     }
-
-//     if (platform.value == "python") {
-//         output.innerHTML = "python request goes here";
+//     for (i = 0; i < x.length; i++) {
+//         x[i].style.display = "none";
 //     }
+//     for (i = 0; i < tabs.length; i++) {
+//         tabs[i].className = tabs[i].className.replace("active", "");
+//     }
+//     x[slideIndex - 1].style.display = "block";
+//     tabs[slideIndex - 1].className += " active ";
 // };
 
 
-//Togle between api responses
 
-const showOk = caller => {
+// // Copy Text
 
-    console.log(caller);
+// const copyText = caller => {
+//     parent = caller.parentElement;
 
-    caller.classList.toggle('active_tab');
-    caller.nextElementSibling.classList.toggle('active_tab');
+//     textarea = parent.previousElementSibling;
 
-    parent = caller.parentNode;
-    parentSibling = parent.nextElementSibling;
-    target = parentSibling.children;
-    ok = target[0];
-    bad = target[1];
+//     textarea.select();
+
+//     textarea.setSelectionRange(0, 99999);
+
+//     document.execCommand("copy");
+
+// };
 
 
-    if (ok.style.display == "none") {
-        ok.style.display = "block";
-        bad.style.display = "none";
-    }
-
-};
+// // Display platform API request based on Language
 
 
 
-const showBad = (caller) => {
+// platform = document.querySelectorAll(".req-platform");
+// platform.forEach(item => {
 
-    caller.classList.toggle('active_tab');
-    caller.previousElementSibling.classList.toggle('active_tab');
+//     item.onchange = () => {
+//         codeEl = item.nextElementSibling;
+//         console.log(codeEl.innerText);
+//         if (item.value == "node") {
+//             console.log(item.value);
+//             codeEl.innerText = "node request goes here";
+
+//         }
+
+//         if (item.value == "php") {
+//             console.log(item.value);
+
+//             codeEl.innerText = "php request goes here";
+//         }
+
+//         if (item.value == "python") {
+//             console.log(item.value);
+//             codeEl.innerText = "python request goes here";
+//         }
+//     };
+// });
+// // console.log(platform);
+
+// // output = document.querySelector(".req__code code");
 
 
-    parent = caller.parentNode;
-    parentSibling = parent.nextElementSibling;
-    target = parentSibling.children;
-    ok = target[0];
-    bad = target[1];
-
-    if (bad.style.display == "none") {
-        bad.style.display = "block";
-        ok.style.display = "none";
-    }
-
-};
+// // platform.onchange = () => {
 
 
+// //     if (platform.value == "node") {
+// //         output.innerHTML = "node request goes here";
+
+// //     }
+
+// //     if (platform.value == "php") {
+// //         output.innerHTML = "php request goes here";
+// //     }
+
+// //     if (platform.value == "python") {
+// //         output.innerHTML = "python request goes here";
+// //     }
+// // };
+
+
+// //Togle between api responses
+
+// const showOk = caller => {
+
+//     console.log(caller);
+
+//     caller.classList.toggle('active_tab');
+//     caller.nextElementSibling.classList.toggle('active_tab');
+
+//     parent = caller.parentNode;
+//     parentSibling = parent.nextElementSibling;
+//     target = parentSibling.children;
+//     ok = target[0];
+//     bad = target[1];
+
+
+//     if (ok.style.display == "none") {
+//         ok.style.display = "block";
+//         bad.style.display = "none";
+//     }
+
+// };
+
+
+
+// const showBad = (caller) => {
+
+//     caller.classList.toggle('active_tab');
+//     caller.previousElementSibling.classList.toggle('active_tab');
+
+
+//     parent = caller.parentNode;
+//     parentSibling = parent.nextElementSibling;
+//     target = parentSibling.children;
+//     ok = target[0];
+//     bad = target[1];
+
+//     if (bad.style.display == "none") {
+//         bad.style.display = "block";
+//         ok.style.display = "none";
+//     }
+
+// };
 
 
 
 
 
-// Toggle API Reference Nested Nav
 
-more = document.querySelector('.more');
+// // Toggle API Reference Nested Nav
 
-more.addEventListener('click', () => {
-    parent = more.parentNode;
-    console.log(parent);
+// more = document.querySelector('.more');
 
-    content = parent.nextElementSibling;
-    if (content.style.display == "none") {
-        content.style.display = "block";
-    } else {
-        content.style.display = "none";
-    }
-    console.log(content);
+// more.addEventListener('click', () => {
+//     parent = more.parentNode;
+//     console.log(parent);
 
-})
+//     content = parent.nextElementSibling;
+//     if (content.style.display == "none") {
+//         content.style.display = "block";
+//     } else {
+//         content.style.display = "none";
+//     }
+//     console.log(content);
+
+// })
