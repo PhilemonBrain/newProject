0b2c514a215c9b56220a83c55702a342953978f5
oscar-ekeyekwu <oscarekeyekwu@gmail.com>
2020-07-30 22:33:56 +0100

    feat: added accounts url file & modified view.py file

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+# import the logging library
+import logging
 
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 # Create your views here.
+
+# signin function
+
+
+def signin(request):
+    logger.info(request.POST)
--- microapi_dashboard/settings.py
+++ microapi_dashboard/settings.py
@@ -92,7 +92,6 @@ DATABASES = {
     }
 }
 
-AUTH_USER_MODEL = "accounts.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
