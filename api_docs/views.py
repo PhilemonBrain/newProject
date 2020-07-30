29bfde1b256f1c3b76df9e113cc9ba12891858b2
Kadet <techkadet@gmail.com>
2020-07-30 11:46:47 +0100

    refactored docs pages

--- api_docs/views.py
+++ api_docs/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 # Views to docs
-def docs(request):
+def commentDocs(request):
     return render(request, 'api_docs/comment_api/comment_doc.html')
 
-# def docs(request):
-#     return render(request, 'api_docs/auth_api/auth_doc.html')
+def authDocs(request):
+    return render(request, 'api_docs/auth_api/auth_doc.html')
 
 # Views to hire consultant
 def consultant(request):
