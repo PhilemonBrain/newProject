9cd7fb17872e5291599e2e9c0f16ab601b04b291
Goodnews Sandy <goodnewssandy@gmail.com>
2020-07-30 10:49:03 +0100

    implemented auth views

--- api_docs/views.py
+++ api_docs/views.py
@@ -6,6 +6,9 @@ from django.shortcuts import render
 def docs(request):
     return render(request, 'api_docs/comment_api/comment_doc.html')
 
+# def docs(request):
+#     return render(request, 'api_docs/auth_api/auth_doc.html')
+
 # Views to hire consultant
 def consultant(request):
     return render(request, 'api_docs/comment_api/api_hire_consultant.html')
