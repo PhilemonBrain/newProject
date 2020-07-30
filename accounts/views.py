0e6a2eaf8b506ca96f0787030081eb49011503a7
oscar-ekeyekwu <oscarekeyekwu@gmail.com>
2020-07-30 23:19:39 +0100

    added request method

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+# import conf from settings
+from django.conf import settings
+# import requests for http requests
+import requests
 # import the logging library
 import logging
 
@@ -10,4 +14,15 @@ logger = logging.getLogger(__name__)
 
 
 def signin(request):
-    logger.info(request.POST)
+    if request.method == "POST":
+        endpoint = '{api_url}/user/login'
+        url = endpoint.format(api_url=settings.AUTH_API_URL)
+        token = 'bearer {api_token}'
+        payload = request.POST
+        headers = token.format(api_token=settings.AUTH_ADMIN_TOKEN)
+        # make http call to endpoint
+        response = requests.post(url, headers=headers, data=payload)
+
+        if response.status_code == 200:
+            result = response.json()
+        return result
