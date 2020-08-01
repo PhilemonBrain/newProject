6771e1c3ca3d2b2cfc0f80f64f40fcfdbd8b04f2
phemmylintry <phemmylintry@gmail.com>
2020-08-01 04:16:38 +0100

    Add api

--- user_dashboard/views.py
+++ user_dashboard/views.py
@@ -8,15 +8,24 @@ from homepage.views import index
 
 @login_required(login_url='/accounts/signin')
 def api_list(request):
-    user = User.objects.all()
-    all_apis = ApiList.objects.order_by('title')[:4]
+    all_apis = ApiList.objects.order_by('title')
     return render(request, 'user_dashboard/dashboard.html', {'all_apis':all_apis})
 
 
-def active_api(request, user, id):
-    if request.method == 'POST':
-        all_api = request.POST['all_api']
-    return render(request, 'user_dashboard/dashboard.html', {'added_apis':added_apis})
+def adding_api(request, id):
+    add_to_api = ApiList.objects.get(id=id)
+    user = request.user
+    print(user)
+    User.add_api(api = add_to_api, name = user)
+    #all_apis = ApiList.objects.order_by('title')
+    return redirect('/dashboard')
+    #return render(request, 'user_dashboard/dashboard.html', {'all_apis':all_apis})
+
+def rmv_api(request, id):
+    add_to_api = ApiList.objects.get(id=id)
+    user = request.user
+    user.api_list.remove(add_to_api)
+    return redirect('/dashboard')
 
 
 @login_required(login_url='/accounts/signin')
